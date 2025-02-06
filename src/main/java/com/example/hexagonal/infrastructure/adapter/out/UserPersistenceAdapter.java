0e90a88357f26c2f6bf07dacfb7fdc62f65a447c
민수아 <Minsua0503@gmail.com>
2025-02-07 01:28:49 +0900

    feat :: UserPersistenceAdapter

---
+++ src/main/java/com/example/hexagonal/infrastructure/adapter/out/UserPersistenceAdapter.java
@@ -0,0 +1,30 @@
+package com.example.hexagonal.infrastructure.adapter.out;
+
+import com.example.hexagonal.application.port.out.UserPort;
+import com.example.hexagonal.domain.model.User;
+import com.example.hexagonal.infrastructure.adapter.out.entity.UserEntity;
+import com.example.hexagonal.infrastructure.adapter.out.mapper.UserMapper;
+import com.example.hexagonal.infrastructure.adapter.out.repository.UserRepository;
+import com.example.hexagonal.infrastructure.global.error.ErrorCode;
+import com.example.hexagonal.infrastructure.global.error.HexagonalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class UserPersistenceAdapter implements UserPort {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public void save(User user) {
+        if(userRepository.existsByAccountId(user.getAccountId())) {
+            throw new HexagonalException(ErrorCode.ALREADY_USER_EXIST);
+        }
+
+        UserEntity userEntity = userMapper.toUserEntity(user);
+        userRepository.save(userEntity);
+    }
+
+}
