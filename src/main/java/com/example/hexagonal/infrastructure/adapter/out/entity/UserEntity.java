6be416143c4626b95ef61a6e24194d52f4e5497b
민수아 <Minsua0503@gmail.com>
2025-02-07 01:29:15 +0900

    feat :: UserEntity

---
+++ src/main/java/com/example/hexagonal/infrastructure/adapter/out/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package com.example.hexagonal.infrastructure.adapter.out.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String accountId;
+
+    @Column(nullable = false)
+    private String password;
+
+}
