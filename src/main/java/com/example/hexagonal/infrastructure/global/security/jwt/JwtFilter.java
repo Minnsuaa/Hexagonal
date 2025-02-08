c0a3a7c548d3b2362ee9c22b996a46ad5b3b0616
민수아 <Minsua0503@gmail.com>
2025-02-09 02:07:07 +0900

    feat :: JwtFilter

---
+++ src/main/java/com/example/hexagonal/infrastructure/global/security/jwt/JwtFilter.java
@@ -0,0 +1,31 @@
+package com.example.hexagonal.infrastructure.global.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtParserAdapter jwtParserAdapter;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String parseToken = jwtParserAdapter.resolveToken(request);
+
+        if(parseToken != null) {
+            Authentication authentication = jwtParserAdapter.authentication(parseToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
