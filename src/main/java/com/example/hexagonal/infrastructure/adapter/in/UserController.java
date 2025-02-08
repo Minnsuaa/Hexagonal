725ea2ace41b97fbc7bac8c00aa39a534574823d
민수아 <Minsua0503@gmail.com>
2025-02-09 02:16:40 +0900

    feat :: LoginUseCase 추가

--- src/main/java/com/example/hexagonal/infrastructure/adapter/in/UserController.java
+++ src/main/java/com/example/hexagonal/infrastructure/adapter/in/UserController.java
@@ -1,7 +1,9 @@
 package com.example.hexagonal.infrastructure.adapter.in;
 
+import com.example.hexagonal.application.port.in.LoginUseCase;
 import com.example.hexagonal.application.port.in.SignupUseCase;
 import com.example.hexagonal.infrastructure.adapter.in.dto.request.UserRequest;
+import com.example.hexagonal.infrastructure.adapter.in.dto.response.TokenResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,11 +15,17 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final SignupUseCase signupUseCase;
+    private final LoginUseCase loginUseCase;
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public void signup(@RequestBody @Valid UserRequest userRequest) {
         signupUseCase.signup(userRequest);
+    }
+
+    @PostMapping("/login")
+    public TokenResponse login(@RequestBody @Valid UserRequest userRequest) {
+        return loginUseCase.login(userRequest);
     }
 
 }
