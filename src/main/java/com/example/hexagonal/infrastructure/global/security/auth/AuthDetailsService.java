0531e75ecec71ccab16a78758e5aa1ce602e0071
민수아 <Minsua0503@gmail.com>
2025-02-09 02:07:21 +0900

    feat :: AuthDetailsService

---
+++ src/main/java/com/example/hexagonal/infrastructure/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.hexagonal.infrastructure.global.security.auth;
+
+import com.example.hexagonal.infrastructure.adapter.out.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
+        return userRepository.findByAccountId(accountId)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(accountId));
+    }
+
+}
