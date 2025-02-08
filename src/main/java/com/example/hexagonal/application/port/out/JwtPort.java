8803b84f4a7a46010442dc78caa7afcce5b9c152
민수아 <Minsua0503@gmail.com>
2025-02-09 02:09:38 +0900

    feat :: JwtPort

---
+++ src/main/java/com/example/hexagonal/application/port/out/JwtPort.java
@@ -0,0 +1,7 @@
+package com.example.hexagonal.application.port.out;
+
+import com.example.hexagonal.infrastructure.adapter.in.dto.response.TokenResponse;
+
+public interface JwtPort {
+    TokenResponse getTokens(String accountId);
+}
