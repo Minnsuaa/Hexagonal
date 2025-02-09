8afd4a41e5cc5eb20797661288124700867a977d
민수아 <Minsua0503@gmail.com>
2025-02-09 17:58:32 +0900

    feat :: findByAccountId

--- src/main/java/com/example/hexagonal/infrastructure/adapter/out/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/hexagonal/infrastructure/adapter/out/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.example.hexagonal.infrastructure.adapter.out.repository;
 import com.example.hexagonal.infrastructure.adapter.out.entity.RefreshTokenEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshTokenEntity, String> {
+    Optional<RefreshTokenEntity> findByAccountId(String accountId);
 }
