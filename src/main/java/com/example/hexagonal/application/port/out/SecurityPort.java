19d6e8b18ceb0de1b932bd8f06d867cd4b1326b9
민수아 <Minsua0503@gmail.com>
2025-02-09 17:55:04 +0900

    feat :: getCurrentUser

--- src/main/java/com/example/hexagonal/application/port/out/SecurityPort.java
+++ src/main/java/com/example/hexagonal/application/port/out/SecurityPort.java
@@ -1,6 +1,13 @@
 package com.example.hexagonal.application.port.out;
 
+import com.example.hexagonal.domain.model.User;
+
 public interface SecurityPort {
+
     String encodePassword(String password);
+
     boolean isPasswordMatch(String password, String encodedPassword);
+
+    User getCurrentUser();
+
 }
--- src/main/java/com/example/hexagonal/infrastructure/global/security/SecurityAdapter.java
+++ src/main/java/com/example/hexagonal/infrastructure/global/security/SecurityAdapter.java
@@ -1,7 +1,12 @@
 package com.example.hexagonal.infrastructure.global.security;
 
 import com.example.hexagonal.application.port.out.SecurityPort;
+import com.example.hexagonal.domain.exception.UserNotFoundException;
+import com.example.hexagonal.domain.model.User;
+import com.example.hexagonal.infrastructure.adapter.out.mapper.UserMapper;
+import com.example.hexagonal.infrastructure.adapter.out.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +15,8 @@ import org.springframework.stereotype.Component;
 public class SecurityAdapter implements SecurityPort {
 
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Override
     public String encodePassword(String password) {
@@ -19,6 +26,14 @@ public class SecurityAdapter implements SecurityPort {
     @Override
     public boolean isPasswordMatch(String password, String encodedPassword) {
         return passwordEncoder.matches(password, encodedPassword);
+    }
+
+    @Override
+    public User getCurrentUser() {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByAccountId(accountId)
+                .map(userMapper::toUser)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 
 }
